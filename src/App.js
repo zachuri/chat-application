6dae67a269a882e211e84edf6ef548fb4e4730b4
zachuri <punsalang.zachary@gmail.com>
2021-12-26 18:19:28 -0800

    start: setup App.js/index.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
+import { ChatEngine } from 'react-chat-engine';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	return (
+		<ChatEngine
+			height="100vh"
+			projectID="90f783b7-8373-4431-a451-ccdaa844422c"
+			userName=""
+			// storing users password
+			userSecret=""
+		/>
+	);
+};
 
 export default App;
