d0c54310df3de3f1ad4c3382380b0497daa122e1
zachuri <punsalang.zachary@gmail.com>
2021-12-26 19:32:53 -0800

    created ChatFeed.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React from 'react';
 import { ChatEngine } from 'react-chat-engine';
 import './App.css';
+import ChatFeed from './components/ChatFeed';
 
 const App = () => {
 	return (
 		<ChatEngine
 			height="100vh"
 			projectID="90f783b7-8373-4431-a451-ccdaa844422c"
-			userName=""
-			// storing users password
-			userSecret=""
+			userName="johnjohn"
+			userSecret="123123"
+			renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
 		/>
 	);
 };
---
+++ src/components/ChatFeed.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ChatFeed = (props) => {
+	console.log(props);
+
+	return <div>ChatFeed</div>;
+};
+
+export default ChatFeed;
